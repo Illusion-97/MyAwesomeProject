288ccd12d5a813fe7fa0bcdb5ca972d6d3aae7ce
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-21 10:13:17 +0100

    Feat : NewCodeWithoutSmell

--- src/main/java/fr/dawan/business/article/ArticleServiceImpl.java
+++ src/main/java/fr/dawan/business/article/ArticleServiceImpl.java
@@ -1,16 +1,19 @@
 package fr.dawan.business.article;
 
 import fr.dawan.business.generic.GenericServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j // Crée automatiquement un logger pour la classe
 public class ArticleServiceImpl extends GenericServiceImpl<Article, ArticleRepository,ArticleDto, ArticleMapper> implements ArticleService {
 
 
     public ArticleServiceImpl(ArticleRepository repository, ArticleMapper mapper) {
         super(repository, mapper);
+        newCodeWithoutSmell();
     }
 
     @Override
@@ -23,7 +26,7 @@ public class ArticleServiceImpl extends GenericServiceImpl<Article, ArticleRepos
         return repository.findByCategory_NameLike("%" + name + "%", pageable).map(mapper::toDto);
     }
 
-    private void NewCodeWithSmell() {
-        System.out.println("This stinks !");
+    private void newCodeWithoutSmell() {
+        log.info("Comme en sortie de boite !");
     }
 }
