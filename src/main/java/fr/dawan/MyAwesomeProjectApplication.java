189ae876cc9ab54689be1a3bee0d4618266a5208
Yanis Adekalom <yanis971.60@gmail.com>
2023-12-21 16:36:35 +0100

    Fix : pom.xml

--- src/main/java/fr/dawan/MyAwesomeProjectApplication.java
+++ src/main/java/fr/dawan/MyAwesomeProjectApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class MyAwesomeProjectApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(MyAwesomeProjectApplication.class, args);
     }
 }
